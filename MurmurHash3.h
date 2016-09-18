1062f78e562de4e4b09961a19e3a370ea924ceb6
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2016-09-17 19:00:30 -0500

    Added 64-bit hash helper functions
    These obtain the 128-bit hash and discard the remaining 64 bits.
    May not be particularly performant on x86 platforms!

--- MurmurHash3.h
+++ MurmurHash3.h
@@ -30,17 +30,39 @@ void MurmurHash3_x86_32(const void * key, int len, uint32_t seed, void * out);
 
 void MurmurHash3_x86_128(const void * key, int len, uint32_t seed, void * out);
 
+inline void MurmurHash3_x86_64(const void * key, int len, uint32_t seed, void * out)
+{
+	uint64_t temp[2];
+	MurmurHash3_x86_128(key, len, seed, &temp);
+	((uint64_t *) out)[0] = temp[0];
+}
+
 void MurmurHash3_x64_128(const void * key, int len, uint32_t seed, void * out);
+
+inline void MurmurHash3_x64_64(const void * key, int len, uint32_t seed, void * out)
+{
+	uint64_t temp[2];
+	MurmurHash3_x64_128(key, len, seed, &temp);
+	((uint64_t *) out)[0] = temp[0];
+}
 
 #if defined(_WIN64) || defined(__x86_64__ ) || defined(__ppc64__)
 inline void MurmurHash3_128(const void * key, int len, uint32_t seed, void *out)
 {
 	return MurmurHash3_x64_128(key, len, seed, out);
 }
+inline void MurmurHash3_64(const void * key, int len, uint32_t seed, void *out)
+{
+	return MurmurHash3_x64_64(key, len, seed, out);
+}
 #else
 inline void MurmurHash3_128(const void * key, int len, uint32_t seed, void *out)
 {
 	return MurmurHash3_x86_128(key, len, seed, out);
+}
+inline void MurmurHash3_64(const void * key, int len, uint32_t seed, void *out)
+{
+	return MurmurHash3_x86_64(key, len, seed, out);
 }
 #endif
 
